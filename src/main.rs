8650c663276b2b8f31d6b2d3f41332680ee2cc7b
Zach Lambert <zachalambert@gmail.com>
2021-08-12 22:06:42 +0100

    Added Display function for Token

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use lexer::Lexer;
 
 mod token;
 use token::Token;
-use token::Constant;
-use token::Keyword;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -25,26 +23,10 @@ fn main() {
 
     let mut lexer = Lexer::new(&mut content);
     let mut tokens : Vec<Token> = Vec::new();
-
     lexer.read_tokens(&mut tokens);
+
     for token in tokens {
-        match token {
-            Token::LParen => println!("{{"),
-            Token::RParen => println!("}}"),
-            Token::LBrace => println!("("),
-            Token::RBrace => println!(")"),
-            Token::Semicolon => println!(";"),
-            Token::Constant(constant) => match constant {
-                Constant::Int(int) => println!("Int({})", int),
-            },
-            Token::Keyword(keyword) => match keyword {
-                Keyword::Int => println!("Keyword(int)"),
-                Keyword::Return => println!("Keyword(return)"),
-            },
-            Token::Identifier(identifier) =>
-                println!("Identifier({})", identifier),
-            Token::Illegal => println!("Illegal"),
-        };
+        println!("{}", token);
     }
     println!("End");
 }
--- src/token/mod.rs
+++ src/token/mod.rs
@@ -1,4 +1,6 @@
 
+use std::fmt;
+
 pub enum Keyword {
     Int,
     Return,
@@ -18,4 +20,27 @@ pub enum Token {
     Keyword(Keyword),
     Identifier(String),
     Illegal,
+}
+
+impl fmt::Display for Token {
+    fn fmt (&self, fmt: &mut std::fmt::Formatter<'_>) -> fmt::Result
+    {
+        match self {
+            Token::LParen => write!(fmt, "{{"),
+            Token::RParen => write!(fmt, "}}"),
+            Token::LBrace => write!(fmt, "("),
+            Token::RBrace => write!(fmt, ")"),
+            Token::Semicolon => write!(fmt, ";"),
+            Token::Constant(constant) => match constant {
+                Constant::Int(int) => write!(fmt, "Int({})", int),
+            },
+            Token::Keyword(keyword) => match keyword {
+                Keyword::Int => write!(fmt, "Keyword(int)"),
+                Keyword::Return => write!(fmt, "Keyword(return)"),
+            },
+            Token::Identifier(identifier) =>
+                write!(fmt, "Identifier({})", identifier),
+            Token::Illegal => write!(fmt, "Illegal"),
+        }
+    }
 }
